5d5f3d49f9cdf6a28e495d5ddec7f2459fb09bce
Byron Ochara <byron.ochara@gmail.com>
2025-10-05 09:24:55 +0300

    created app layout with nested links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orerId", element: <Order /> },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      Hellloooo.... welcome to our next steps of creating real-world projects!!!
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
