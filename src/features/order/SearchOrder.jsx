490ffa3437294dbad87fea08c60c7d36d4d0f844
Byron Ochara <byron.ochara@gmail.com>
2025-10-05 11:28:48 +0300

    created SearchOrder input component

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    //this is to prevent the page from reloading and navigating to another link or page
+    e.preventDefault();
+    // Search logic 
+    if(!query) return;
+    navigate(`/order/${query}`);
+    setQuery(""); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your order ID"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
