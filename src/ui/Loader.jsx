b975a01d240aee76e47850ec62b24f444571fe54
Byron Ochara <byron.ochara@gmail.com>
2025-10-06 11:25:45 +0300

    applied tailwind styles on loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function Loader() {
-  return <div className="loader">
-    <p>Loading...</p>
+  return <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+    <div className="loader"></div>
   </div>;
 }
 
