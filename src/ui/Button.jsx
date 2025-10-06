7836ce3eee7e1cb0f73c3facc7a8f62094e5ad71
Byron Ochara <byron.ochara@gmail.com>
2025-10-06 11:06:57 +0300

    updated button to contain link button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Button({ children, disabled }) {
+function Button({ children, disabled, to }) {
+  const className =
+    'inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4';
+  
+    if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
