2b0d6b3040579dc8d13671890cfde891855f53e4
Byron Ochara <byron.ochara@gmail.com>
2025-10-06 09:44:06 +0300

    added 2 more styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ function Button({ children, disabled }) {
   return (
     <button
       disabled={disabled}
-      className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+      className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4"
     >
       {children}
     </button>
