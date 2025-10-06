ccf9f47a42603abd5ba938d34f07e0772b18c4b5
Byron Ochara <byron.ochara@gmail.com>
2025-10-06 15:58:18 +0300

    updated Button dynamic styling by type

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4';
-  
-    if (to)
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full transition-color text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2';
+
+  const styles = {
+    primary: base + 'px-4 py-3 md:px-6 md:py-4',
+    small: base + 'px-4 py-2 md:px-5 md:py-2.5 text-xs',
+  };
+
+  if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
